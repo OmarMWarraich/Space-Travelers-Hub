4ef14bab82f9747084ea223c0a2479c844d48e3d
OmarMWarraich <earthling986@gmail.com>
2022-10-11 03:25:21 +0500

    chore: Add dependencies and routes

--- src/features/counter/counterAPI.js
+++
@@ -1,5 +0,0 @@
-// A mock function to mimic making an async request for data
-/* eslint-disable-next-line import/prefer-default-export */
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) => setTimeout(() => resolve({ data: amount }), 500));
-}
