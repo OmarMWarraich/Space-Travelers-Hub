59fb96d431e2e126939ce10d843cefd1d8cfedb2
OmarMWarraich <earthling986@gmail.com>
2022-10-11 03:07:14 +0500

    Initial Commit: cra and linters

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+/* eslint-disable-next-line import/prefer-default-export */
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
