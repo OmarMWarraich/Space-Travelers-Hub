58f2eb21aa16ade97308b899807a5c892794e4d8
OmarMWarraich <earthling986@gmail.com>
2022-10-14 04:50:11 +0500

    chore: Resolve conflict files

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import missionsReducer from '../features/missions/missionsSlice';
-
-/* eslint-disable-next-line import/prefer-default-export */
-export const store = configureStore({
-  reducer: {
-    missions: missionsReducer,
-  },
-});
